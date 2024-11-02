463656d925c5be7adfcdeca85dae96203c4a032c
austinkaylor-eaton <austinjkaylor@eaton.com>
2024-11-01 21:49:07 -0400

    Committing what I have now

---
+++ Database/Entities/v2/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace Database.Entities.v2;
+
+/// <summary>
+/// A category of products in the eCommerce store
+/// </summary>
+public class Category
+{
+    public int Id { get; set; }
+    /// <summary>
+    /// The name of the category
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// The description of the category
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+    /// <summary>
+    /// The Id of the parent of this category
+    /// </summary>
+    public int ParentCategoryId { get; set; }
+}
---
+++ Database/Entities/v2/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Database.Entities.v2;
+
+/// <summary>
+/// A product in the eCommerce store
+/// </summary>
+public class Product
+{
+    public long Id { get; set; }
+    /// <summary>
+    /// Foreign key to the <see cref="Category"/> entity
+    /// </summary>
+    public int CategoryId { get; set; }
+    /// <summary>
+    /// A Product's <see cref="Category"/>
+    /// </summary>
+    public Category? Category { get; set; }
+    /// <summary>
+    /// The name of the product
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// The description of the product
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+}
